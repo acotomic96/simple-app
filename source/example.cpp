805cf03b24d80141d66ad145eea22472f1d636b4
Aleksandar <aleksandar.tomic@syrmia.com>
2021-10-06 11:54:36 +0200

    hohoh

--- source/example.cpp
+++ source/example.cpp
@@ -1,5 +1,5 @@
 #include "example.h"
-
+//hohohoho
 ///////////////////////////////////////////////Encryption///////////////////////////////////////////////////
 void encrypt(uint_least8_t* iData, int_least32_t iWidth, int_least32_t iHeight, char mes[], const char key[])
 {
